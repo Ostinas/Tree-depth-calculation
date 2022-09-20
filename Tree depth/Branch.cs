7100453ab6b942c093fbeeb62d198c57f8eb856a
Ostinas <ostinasmaciulis@gmail.com>
2022-09-20 16:28:36 +0300

    Add project files.

---
+++ Tree depth/Branch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_depth
+{
+    internal class Branch
+    {
+        private Branch Parent { get; set; }
+        private List<Branch> Branches { get; set; }
+
+        public Branch()
+        {
+            Branches = new List<Branch>();
+        }
+
+        public void Add(Branch branch)
+        {
+            branch.Parent = this;
+            Branches.Add(branch);
+        }
+        private int TreeHeight(Branch parent, int depth)
+        {
+            int result = depth;
+            foreach (var branch in parent.Branches)
+            {
+                result = Math.Max(result, TreeHeight(branch, depth + 1));
+            }
+            return result;
+        }
+        
+        public int GetHeight()
+        {
+            return TreeHeight(this, 0);
+        }
+    }
+}
---
+++ Tree depth/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tree_depth;
+
+Branch tree = new Branch();
+Branch branch1 = new Branch();
+Branch branch2 = new Branch();
+Branch branch3 = new Branch();
+Branch branch4 = new Branch();
+Branch branch5 = new Branch();
+Branch branch6 = new Branch();
+Branch branch7 = new Branch();
+Branch branch8 = new Branch();
+Branch branch9 = new Branch();
+branch7.Add(branch8);
+branch4.Add(branch7);
+branch4.Add(branch9);
+branch3.Add(branch6);
+branch1.Add(branch5);
+branch1.Add(branch3);
+branch1.Add(branch4);
+tree.Add(branch1);
+tree.Add(branch2);
+Console.WriteLine(tree.GetHeight());
+
+Branch LinearTree = new Branch();
+Branch LinearBranch = new Branch();
+Branch LinearBranch2 = new Branch();
+Branch LinearBranch3 = new Branch();
+Branch LinearBranch4 = new Branch();
+Branch LinearBranch5 = new Branch();
+LinearBranch4.Add(LinearBranch5);
+LinearBranch3.Add(LinearBranch4);
+LinearBranch2.Add(LinearBranch3);
+LinearBranch.Add(LinearBranch2);
+LinearTree.Add(LinearBranch);
+Console.WriteLine(LinearTree.GetHeight());
